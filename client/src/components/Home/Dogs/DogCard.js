fc28e58cecb72f41edc023ac3a9d01fef402a659
lucasibar <lucas.ibar.soy.henry@gmail.com>
2023-02-06 15:21:41 -0300

    css del home casi terminado

--- client/src/components/Home/Dogs/DogCard.js
+++ client/src/components/Home/Dogs/DogCard.js
@@ -3,16 +3,17 @@ import './DogCard.css'
 import { NavLink } from "react-router-dom";
 
 
-export default function DogCard({ id, name, img, temperament}){
+export default function DogCard({ id, name, img}){
 
 return(
 <div className='carta'>
-    <NavLink className="namelink" to={`/detail/${id}`}>
-    <img className="imgCarta" src={img} alt="imagen de raza"/>
-    <div className='texto'>{name}</div>   
+        <div className='ciculo'></div>
+        <img className='imgCarta' src={img} alt="imagen de raza"/>
+        
+    <NavLink className="linkDetalle" to={`/detail/${id}`}>
+    {name}   
     </NavLink>	
-
-
+    <div className='ciculoRelleno'></div>  
 </div>
 )
 }
